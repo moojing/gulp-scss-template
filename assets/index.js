f5c843a799c24f5db46a91d70cb542fae790ab18
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-02-01 11:12:37 +0800

    finish cal template

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+console.log('ijdex');
+
+
+
+function insert(){
+    document.form.calView.vaule
+    console.log('document.form.calView.vaule: ', document.form.calView.vaule);
+} 
+
+function equql(){
+    var exp = document.form.calView.value
+} 
